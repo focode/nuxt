300d5cea6fe8389e8f81fdc812e39d3eb8c4aedb
Dubey <arunesh.dubey@siemens.com>
2018-05-15 19:46:09 +0530

    why choose us

--- pages/ChooseUs.vue
+++ pages/ChooseUs.vue
@@ -1,27 +1,64 @@
 <template>
-  <v-layout column>
-    <v-flex xs12 sm6 offset-sm3>
-      
-      <v-card>
-     SoftKiwi Technologies undertake a holistic approach towards each project that we work on. So we put a lot of hard work in understanding the real needs and addressing them.
-
-Right from research, to probing into business objectives of our clients, we do a significant deliberation intended at a precisely successful outcome.
-
-Our commitment is towards accomplishing high levels of customer contentment, which comes by achieving great quality in the outcome, providing timely solutions and fulfilling the needs of the businesses pertaining to communication with their clients/customers.
-
-What makes us different.
-Expertise
-Our qualified, professional teams are made up of the industry’s best and brightest software engineers.
-
-Experience
-Our extensive experience spans across public and private sector firms of all sizes. We understand business processes and technologies and the challenges you face.
-
-Research and Development
-Our teams are continually researching new and proven technologies to benefit our clients.
-
-Affordability
-SoftKiwi Technologies are dedicated to provide you high quality software services to deliver robust, scalable, and cost-effective solutions within your budget
-      </v-card>
-    </v-flex>
-  </v-layout>
+<div>
+  
+    <div class="ex1">
+     <strong> <h1> Why Choose Us </h1> </strong> </div>
+  <div class="wrapper">
+  <article>
+   
+    <p>
+      Adore Software Technologies undertake a holistic approach towards each project that we work on. So we put a lot of hard work in understanding the real needs and addressing them..
+    </p>
+    <p>
+      Right from research, to probing into business objectives of our clients, we do a significant deliberation intended at a precisely successful outcome.
+    </p>
+    <p>
+      Our commitment is towards accomplishing high levels of customer contentment, which comes by achieving great quality in the outcome, providing timely solutions and fulfilling the needs of the businesses pertaining to communication with their clients/customers.
+    </p>
+    <p>
+      <strong>What makes us different. </strong>
+    </p>
+    <p>
+      <strong>Expertise. </strong>
+    </p>
+    <p>
+      Our qualified, professional teams are made up of the industry’s best and brightest software engineers.
+    </p>
+     <p>
+      <strong>Experience. </strong>
+    </p>
+    <p>
+      Our extensive experience spans across public and private sector firms of all sizes. We understand business processes and technologies and the challenges you face.
+    </p>
+    <p>
+      <strong>Research and Development</strong>
+    </p>
+    <p>
+      Our teams are continually researching new and proven technologies to benefit our clients.
+    </p>
+    <p>
+      <strong>
+        Affordability
+      </strong>
+    </p>
+    <p>
+      Adore Software Technologies are dedicated to provide you high quality software services to deliver robust, scalable, and cost-effective solutions within your budget
+    </p>
+  </article>
+  </div>
+</div>
 </template>
+<style scoped>
+.p {
+  padding: 1em;
+  }
+.wrapper {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  max-width: 1200px;
+}
+div.ex1 {
+    margin-left: 300px;padding: 2em;
+}
+
+</style>
